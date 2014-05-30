67f545d5a8a2f478da622e1c7a2d59af6ef2f939
matthew <matthew@gmail.com>
2014-05-30 22:02:35 +0800

    add 2 more member fields to Crime.java mDate and mSolved

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,39 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
+	
+	private Date mDate;
+	private boolean mSolved;
+	
+	
+
 	private UUID mId;
 	private String mTitle;
 	
 	public Crime(){
+		
+		mDate = new Date();
+		
 		//generate unique identifier
 		mId = UUID.randomUUID();
+	}
+	
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
 	}
 
 	public String getTitle() {
