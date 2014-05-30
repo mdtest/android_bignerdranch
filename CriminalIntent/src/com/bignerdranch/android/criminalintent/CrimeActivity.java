a9983c2a96dc17de8160e79b7440f75bbc92027e
matthew <matthew@gmail.com>
2014-05-30 21:15:13 +0800

    created a new activity: CrimeActivity

---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CrimeActivity extends FragmentActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+}
