c617dd2d6a9db473589ba2a6f89d8c1020a8a63c
matthew <matthew@gmail.com>
2014-05-31 14:11:34 +0800

    create our own row layout by introducing our own CrimeAdapter new list_item_crime.xml for all rows modified the content of CrimeListFragment but cannot click on the item in the list but by making the checkbox property focusable to false in list_item_crime.xml we can click items again

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 	private static String TAG = "CrimeListFragment";
@@ -24,7 +27,9 @@ public class CrimeListFragment extends ListFragment {
 		//Toast.makeText(getActivity().getBaseContext(),  "getActivity() returns: " + getActivity().toString(),  Toast.LENGTH_LONG).show();
 		//end of debug message section-----------------
 		
-		ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
+		//ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
+		//our own CrimeAdapter
+		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
 		
 		
@@ -32,7 +37,37 @@ public class CrimeListFragment extends ListFragment {
 	
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id){
-		Crime c = (Crime)getListAdapter().getItem(position);
+		//Crime c = (Crime)getListAdapter().getItem(position);
+		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 		Log.d(TAG, c.getTitle() + " was clicked");
+	}
+	
+	private class CrimeAdapter extends ArrayAdapter<Crime>{
+		public CrimeAdapter(ArrayList<Crime> crimes){
+			super(getActivity(), 0, crimes);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent){
+			//if we weren't given a view, inflate one
+			if(convertView == null){
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime,  null);
+								
+			}
+			
+			//configure the view for this Crime
+			Crime c = getItem(position);
+			
+			TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getTitle());
+			
+			TextView dateTextView = (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText(c.getDate().toString());
+			
+			CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.isSolved());			
+			
+			return convertView;
+		}
 	}
 }
