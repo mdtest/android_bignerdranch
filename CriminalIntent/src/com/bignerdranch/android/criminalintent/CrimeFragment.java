7734820e45ecaa513600a9bc3b9d088e1772f6f4
matthew <matthew@gmail.com>
2014-05-31 14:31:32 +0800

    direct retrieval intent intent extra is define in CrimeFragment.java

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.UUID;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -15,6 +17,9 @@ import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	
+	public static final String EXTRA_CRIME_ID = 
+			"com.bignerdranch.android.criminalintent.crime_id";
+	
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
 	
@@ -24,7 +29,9 @@ public class CrimeFragment extends Fragment {
 	@Override 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		mCrime = new Crime();		
+		//mCrime = new Crime();
+		UUID crimeId = (UUID)getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
 	
 	@Override
@@ -32,6 +39,7 @@ public class CrimeFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		
 		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
 		mTitleField.addTextChangedListener(new TextWatcher(){
 			public void onTextChanged(CharSequence c, int start, int before, int count){
 				mCrime.setTitle(c.toString());
@@ -52,6 +60,7 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setEnabled(false);
 		
 		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
 				mCrime.setSolved(isChecked);
