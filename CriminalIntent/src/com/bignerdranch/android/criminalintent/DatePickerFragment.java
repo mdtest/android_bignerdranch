967c2c0559978f4ee72193567b2e96a11c37b12a
matthew <matthew@gmail.com>
2014-06-06 16:21:53 +0800

    calling back to your target in DatePickerFragment.java

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,8 +4,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
@@ -17,6 +20,17 @@ public class DatePickerFragment extends DialogFragment {
 	public static final String EXTRA_DATE = "com.bignerdranch.android.criminalintent.date";
 	
 	private Date mDate;
+	
+	private void sendResult(int resultCode){
+		if(getTargetFragment() == null)
+			return;
+		
+		Intent i = new Intent();
+		i.putExtra(EXTRA_DATE,  mDate);
+		
+		getTargetFragment().onActivityResult(getTargetRequestCode(),  resultCode,  i);
+	}
+	
 	public static DatePickerFragment newInstance(Date date){
 		Bundle args = new Bundle();
 		args.putSerializable(EXTRA_DATE,  date);
@@ -55,7 +69,14 @@ public class DatePickerFragment extends DialogFragment {
 		return new AlertDialog.Builder(getActivity())
 								.setView(v)
 								.setTitle(R.string.date_picker_title)
-								.setPositiveButton(android.R.string.ok, null)
+								//.setPositiveButton(android.R.string.ok, null)
+								.setPositiveButton(android.R.string.ok,
+										new DialogInterface.OnClickListener(){
+									public void onClick(DialogInterface dialog, int which){
+										sendResult(Activity.RESULT_OK);
+									}
+									
+								})
 								.create();
 	}
 	
