c736e6e560217eaeb9017a8044a9d2f4ac398a64
matthew <matthew@gmail.com>
2014-06-06 16:04:51 +0800

    add call to newInstance() in CrimeFragment.java

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -74,7 +74,9 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
 				FragmentManager fm = getActivity().getSupportFragmentManager();
-				DatePickerFragment dialog = new DatePickerFragment();
+				//DatePickerFragment dialog = new DatePickerFragment();
+				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+				
 				dialog.show(fm, DIALOG_DATE);
 			}
 		});
